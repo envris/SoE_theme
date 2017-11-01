a69a446d4f778fa394a3c7ae6bcca30f6a93af19
Joseph Henriquez <jhenriquez@doghouse.agency>
2017-11-01 15:44:57 +0800

    Add functionality to a/b testing

--- doesoe_theme/js/script.js
+++ doesoe_theme/js/script.js
@@ -1253,4 +1253,28 @@
     }
   };
 
+  /*
+   * Show topic results on click.
+   */
+  Drupal.behaviors.showTopic = {
+    attach: function (context, settings) {
+      $('.js-select-all').click(function() {
+        $(".view-display-id-page_b .view-content, .item-list").show();
+      });
+    }
+  };
+
+  /*
+   * Show topic results on form submission.
+   */
+  Drupal.behaviors.showTopicOnSubmit = {
+    attach: function (context, settings) {
+      $(document).ajaxComplete(function(event, jqXHR, ajaxOptions) {
+        if (ajaxOptions.extraData.view_name == 'topics') {
+          $(".view-display-id-page_b .view-content, .item-list").show();
+        }
+      });
+    }
+  };
+
 })(jQuery, Drupal, this, this.document);
